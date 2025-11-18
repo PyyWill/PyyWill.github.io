ae9c8562082568fa8e21a52393600183d39a62d9
unknown <pyy030406@sjtu.edu.cn>
2025-11-18 15:14:23 -0500

    Remove visit counter functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono, Noto_Serif, PT_Serif } from "next/font/google";
 import "./globals.css";
 import { aboutMe } from "@/data/aboutme";
 import { customMetadata } from "@/data/title-description";
-import { VisitCounter } from "@/components/visit-counter";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -46,32 +45,28 @@ export default function RootLayout({
       >
         <main className="">{children}</main>
         <footer className="border-t border-neutral-200 dark:border-neutral-800 bg-[#FFFCF8]">
-          <div className="flex flex-col mx-auto max-w-7xl px-6 py-12">
-            <div className="flex flex-row md:flex md:items-start md:justify-between mb-8">
-              <div className="mb-4 text-sm text-neutral-600 dark:text-neutral-400">
-                <p>
-                  © {new Date().getFullYear()} {aboutMe.name}.
+          <div className="flex flex-row mx-auto max-w-7xl px-6 py-12 md:flex md:items-start md:justify-between ">
+            <div className="mb-4 text-sm text-neutral-600 dark:text-neutral-400">
+              <p>
+                © {new Date().getFullYear()} {aboutMe.name}.
+              </p>
+              {aboutMe.secretDescription && (
+                <p className="text-xs text-neutral-600 dark:text-neutral-400 mt-4">
+                  {aboutMe.secretDescription}
                 </p>
-                {aboutMe.secretDescription && (
-                  <p className="text-xs text-neutral-600 dark:text-neutral-400 mt-4">
-                    {aboutMe.secretDescription}
-                  </p>
-                )}
-              </div>
-              <div className="mb-4">
-                <p className="text-sm text-neutral-500 dark:text-neutral-500 justify">
-                  Built with{" "}
-                  <a
-                    href="https://github.com/tovacinni/research-website-template"
-                    className="underline hover:text-neutral-800 dark:hover:text-neutral-300 transition-colors"
-                  >
-                    research-website-template
-                  </a>
-                </p>
-              </div>
+              )}
             </div>
-            {/* Visit Counter */}
-            <VisitCounter />
+            <div className="mb-4">
+              <p className="text-sm text-neutral-500 dark:text-neutral-500 justify">
+                Built with{" "}
+                <a
+                  href="https://github.com/tovacinni/research-website-template"
+                  className="underline hover:text-neutral-800 dark:hover:text-neutral-300 transition-colors"
+                >
+                  research-website-template
+                </a>
+              </p>
+            </div>
           </div>
         </footer>
       </body>
